80fcfc2b03ccc450421b7a3105f2415ae95c56ea
Humayun63 <humayun126224@gmail.com>
2023-05-30 20:17:08 +0600

    provider added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzUJcqLzy5FREc7VCxRMMYmPXwce9d6Wo",
+  authDomain: "doc-house-15aa4.firebaseapp.com",
+  projectId: "doc-house-15aa4",
+  storageBucket: "doc-house-15aa4.appspot.com",
+  messagingSenderId: "378728025314",
+  appId: "1:378728025314:web:941a4ce9611bff856522bb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
