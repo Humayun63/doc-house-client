7a35dc9f078291ae5efdd35af9e85c90fed1fa5c
Humayun63 <humayun126224@gmail.com>
2023-05-30 20:21:52 +0600

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import AuthProvider from './providers/AuthProvider'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/Router'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Toaster />
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
